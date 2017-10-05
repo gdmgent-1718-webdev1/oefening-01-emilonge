1699a8f7fd99fb866f4c5c8d9d064d0ce3746280
emilonge <emilonge@student.arteveldehs.be>
2017-10-05 09:06:21 +0200

    “update”

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oefening 1 - Emily Ongenaert</title>
+</head>
+<body>
+    <?php
+    $naam = 'Emily';
+    ?>
+    <h1>Welkom, <?= $naam ?></h1>
+</body>
+</html>
